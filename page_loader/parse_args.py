93ddf2dc07695145deef8fae9bb25c96418fef22
mrJonson <mr.Jonson@mail.ru>
2022-03-01 13:12:01 +0400

    add files

---
+++ page_loader/parse_args.py
@@ -0,0 +1,12 @@
+import argparse
+import os
+
+
+def cli_parser():
+    parser = argparse.ArgumentParser(description='Page loader')
+    parser.add_argument('-o', '--output',
+                        default=os.getcwd(), type=str,
+                        help='folder for saving html')
+    parser.add_argument('URL', type=str,
+                        help='url to parse from')
+    return parser.parse_args()
